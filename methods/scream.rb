0539c73f44f416a9a774eb4fc4e5574cecf0f754
Nirzhar <nirzhar_mp@yahoo.com>
2017-02-20 14:13:19 -0800

    Added return value and comment

--- methods/scream.rb
+++ methods/scream.rb
@@ -3,5 +3,5 @@
 def scream(words)
   words = words + "!!!!"
 end
-
+"hello there" #this will return hello there as the return value. The last thing in a method is the return value. 
 puts scream("YIPEEE")
